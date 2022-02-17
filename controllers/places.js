cd2d84b8e657f5e700eff8b96e4c36fb8717128d
Lisa Brown <lisaann493@verizon.net>
2022-02-16 20:13:13 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -29,5 +29,14 @@ router.get('/', (req, res) => {
   res.render('places/index', {places})
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id]})
+  }
+})
 // Export router
 module.exports = router
