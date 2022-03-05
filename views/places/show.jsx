0b42a63e8bfb4a3504fcf07b80231367c31db58c
Lisa Brown <lisaann493@verizon.net>
2022-03-05 14:43:26 -0500

    Added rating calculations

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -10,7 +10,21 @@ function show (data) {
       No comments
     </h4>
   )
+  let rating = (
+    <h3 className='inactive'>
+      Not rated
+    </h3>
+  )
   if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = sumRatings / data.place.comments.length
+    rating = (
+      <h3>
+        {Math.round(averageRating)} stars
+      </h3>
+    )
     comments = data.place.comments.map(c => {
       return (
         <div className='border'>
@@ -37,7 +51,7 @@ function show (data) {
                 <h4>Serving {data.place.cuisines}</h4>
                 <br></br>
                 <h2>Rating</h2>
-                <h4>Not Rated</h4>
+                <h4>{rating}</h4>
               </div>
               <div className='col'>
                 <img src={data.place.pic} alt={data.place.name}></img>
